730175c4622c7add7b098e2326e0b4659ab18f89
Marzouki Tasnim <tasnim.marzouki@bizerte.r-iset.tn>
2026-05-08 23:54:25 +0200

    feat: add Dockerfile closes#5

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,30 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+
+  mode: 'development',
+
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
+
   devServer: {
-    static: './dist',
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 8080,
+    open: true,
+    hot: true,
   },
+
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+
   module: {
     rules: [
       {
@@ -24,5 +35,4 @@ module.exports = {
       },
     ],
   },
-  mode: 'production',
 };
